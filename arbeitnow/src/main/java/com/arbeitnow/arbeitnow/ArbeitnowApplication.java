d4f9e30099af4f6e9aedcc38ee757e50da854247
Oleg Shkoloviy <shkoloviy392@gmail.com>
2024-07-23 16:13:47 +0300

    setup

---
+++ arbeitnow/src/main/java/com/arbeitnow/arbeitnow/ArbeitnowApplication.java
@@ -0,0 +1,13 @@
+package com.arbeitnow.arbeitnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbeitnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbeitnowApplication.class, args);
+	}
+
+}
---
+++ arbeitnow/src/test/java/com/arbeitnow/arbeitnow/ArbeitnowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arbeitnow.arbeitnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArbeitnowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
